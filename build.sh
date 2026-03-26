01ea7181695b39aab64ac8653881680e7b068b35
mowaiskhan147-afk <mowaiskhan147@gmail.com>
2026-03-26 12:08:01 +0500

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-export PATH="$HOME/.cargo/bin:$PATH"
-
-# Install Python dependencies
-pip install -r requirements.txt

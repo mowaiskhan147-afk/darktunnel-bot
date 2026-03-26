22190fe2afab7848b3342836de4988dc4cc57fb1
Owais <mowaiskhan147@gmail.com>
2026-03-26 11:35:25 +0500

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-chmod +x build.sh#!/bin/bash
+#!/bin/bash
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
+
 # Install Python dependencies
 pip install -r requirements.txt
